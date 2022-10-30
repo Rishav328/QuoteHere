7d91f83654eb58dc00743e0b62db98ec2f979a82
Aishwaray(Rishav)Tiwary <70231128+Rishav328@users.noreply.github.com>
2022-10-30 20:21:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const HEIGHT = window.innerHeight;
+const WIDTH  = window.innerWidth;
+
+$("#quo").hide()
+
+if (WIDTH <= 850) {
+
+    var app = document.getElementById('quo');
+
+    var typewriter = new Typewriter(app, {
+        loop: true
+    });
+
+    typewriter.pauseFor(2000)
+        .typeString(`"Share your words 😎!"`)
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString(`"Get rewarded with Upvotes ❤️!"`)
+        .start();
+
+    setTimeout(()=>{$("#quo").show()},1800)
+
+}
+
+$('.nav').hide();
+$('.nav').slideDown(1000);
+$('#body-quo').hide();
+setTimeout(()=>{$('#body-quo').fadeIn(1000)}, 900)
+
+
